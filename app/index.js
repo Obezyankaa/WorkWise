e2768e62fe583a994b5c266e2cc4b17117d12129
VladAir <vladair@MacBook-Air-VladAir.local>
2023-04-18 17:00:42 +0300

    запустили проект

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import { Link, Stack } from "expo-router";
+
+export default function Home() {
+  return (
+      <View>
+          <Text>
+              Home
+          </Text>
+   </View>
+  );
+}
