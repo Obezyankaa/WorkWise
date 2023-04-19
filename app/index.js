b3298ebe2d2ceddd8420c4849aa5747871b7acdb
VladAir <vladair@MacBook-Air-VladAir.local>
2023-04-19 11:32:06 +0300

    сделал навигацию header

--- app/index.js
+++ app/index.js
@@ -1,12 +1,53 @@
-import { View, Text } from "react-native";
-import { Link, Stack } from "expo-router";
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components"
 
 export default function Home() {
+  const router = useRouter()
+
+   const handlePress = () => {
+     console.log(123);
+   };
   return (
-      <View>
-          <Text>
-              hello
-          </Text>
-   </View>
+    // оборачивает приложение в SafeAreaView
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: COLORS.lightWhite,
+          },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.menu}
+              dimension="60%"
+              handlePress={handlePress}
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={imagesx.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+      {/* можно будет скролить и не показывать индикатор прокрутки */}
+      <ScrollView showsHorizontalScrollIndicator={false}>
+        {/* показывает вот это  */}
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          {/* наши компоненты  */}
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
