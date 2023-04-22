93033d33c4c4673b918dd20fe0fa5238634cf967
VladAir <vladair@MacBook-Air-VladAir.local>
2023-04-22 16:54:24 +0300

    выводятся популярные вакансии в консоль

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -3,15 +3,20 @@ import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react
 import { useRouter } from "expo-router";
 import { COLORS, SIZES } from '../../../constants';
 import PopularJobCard from '../../common/cards/popular/PopularJobCard';
+import useFetch from '../../../hook/useFetch';
 
 
 import styles from './popularjobs.style'
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false; 
-  const error = false;
 
+  const { data, isLoading, error } = useFetch("search", {
+    query: "Python developer in Texas, USA",
+    num_pages: "1",
+  });
+
+  console.log('Popularjobs ---->>', data);
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -21,19 +26,20 @@ const Popularjobs = () => {
         </TouchableOpacity>
       </View>
       <View style={styles.cardsContainer}>
-        {isLoading ?
-          (<ActivityIndicator size="large" color={COLORS.primary} />)
-          : error ? (<Text>Something went wrong</Text>)
-            : (<FlatList
-              data={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
-              renderItem={({item}) => (
-                <PopularJobCard item={item} />
-              )}
-              keyExtractor={item => item?.job_id}
-              contentContainerStyle={{ columnGap: SIZES.medium }}
-              horizontal
-            />
-            )}
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList
+            showsHorizontalScrollIndicator={false}
+            data={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+            renderItem={({ item }) => <PopularJobCard item={item} />}
+            keyExtractor={(item) => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
       </View>
     </View>
   );
